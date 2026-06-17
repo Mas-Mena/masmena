4df517d1ca27e83a094aee1aa434233d0708fc8e
motasemodeh <motasem.udeh@gmail.com>
2026-06-17 15:34:01 +0300

    fix: scale img instead of container in testimonial hover to prevent border shifts

--- src/components/sections/EditorialTestimonials.tsx
+++ src/components/sections/EditorialTestimonials.tsx
@@ -68,11 +68,11 @@ const EditorialTestimonials: React.FC = () => {
               className="relative aspect-auto min-h-[420px] md:min-h-0 md:aspect-[1.15/1] bg-black overflow-hidden group flex flex-col md:block"
             >
               {/* Background Image - monochromatic & desaturated */}
-              <div className="relative md:absolute top-0 left-0 right-0 h-[200px] sm:h-[240px] md:h-[65%] grayscale contrast-125 brightness-75 transition-all duration-[1.2s] ease-in-out group-hover:scale-105 group-hover:grayscale-0 group-hover:contrast-100 group-hover:brightness-90 flex-shrink-0">
+              <div className="relative md:absolute top-0 left-0 right-0 h-[200px] sm:h-[240px] md:h-[65%] overflow-hidden grayscale contrast-125 brightness-75 transition-all duration-[1.2s] ease-in-out group-hover:grayscale-0 group-hover:contrast-100 group-hover:brightness-90 flex-shrink-0">
                 <img 
                   src={testimonial.image} 
                   alt={testimonial.author} 
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-cover transition-transform duration-[1.2s] ease-in-out group-hover:scale-105"
                 />
                 {/* Vignette to blend into bottom */}
                 <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black" />
